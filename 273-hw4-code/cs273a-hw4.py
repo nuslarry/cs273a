f7e417434ea967160abda675f22be1b5cbd808f3
larry1285 <larry1285@gmail.com>
2019-02-28 22:24:28 -0800

    sourceTree test

--- 273-hw4-code/cs273a-hw4.py
+++ 273-hw4-code/cs273a-hw4.py
@@ -133,12 +133,14 @@ plt.show()
 
 #question 2.6
 dt = ml.dtree.treeClassify(xt_0_10000, yt_0_10000, maxDepth = 9)
-test = dt.roc(xt_0_10000, yt_0_10000)
+
+yt_0_10000_hat = dt.predict(xt_0_10000)
+test = dt.roc(yt_0_10000, yt_0_10000_hat)
 print(type(test))
 print(test.shape)
 
 
-
+#sourceTree test
 
 
 
